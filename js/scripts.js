803f1c8c14b73e2fa058c56d9bb0fc1759752b11
yashantha kumar <yashvanth1590@gmail.com>
2020-09-05 10:24:14 +0530

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+            $('#mycarousel').carousel({interval:2000});
+            $('#carouselButton').click(function() {
+                if($('#carouselButton').children('span').hasClass('fa-pause')) {
+                    $('#mycarousel').carousel('pause');
+                    $('#carouselButton').children('span').removeClass('fa-pause');
+                    $('#carouselButton').children('span').addClass('fa-play');
+                }
+                else {
+                    $('#mycarousel').carousel('cycle');
+                    $('#carouselButton').children('span').removeClass('fa-play');
+                    $('#carouselButton').children('span').addClass('fa-pause');
+                }
+            });
+            $('#reserveButton').click(function () {
+                $('#reserveModal').modal('show');
+            });
+            $('#loginButton').click(function () {
+                $('#loginModal').modal('show');
+            });
+        })
